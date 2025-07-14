b641951945b2a58dc5b709833da200f69ff22ece
May0Mei <67112215+May0Mei@users.noreply.github.com>
2025-07-14 22:02:02 +0000

    Deploying to gh-pages from @ May0Mei/May0Mei.github.io@c2dd7d8c9119b237497ff7a672f9a6e2047b13d0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "Projects",
-          description: "A collection of my cool side projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
@@ -36,6 +29,13 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
+          },
+        },{id: "nav-side-projects",
+          title: "Side Projects",
+          description: "A collection of my cool side projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
           },
         },{id: "post-a-post-with-plotly-js",
         
